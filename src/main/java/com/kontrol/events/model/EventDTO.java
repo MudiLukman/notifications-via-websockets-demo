92304e0132649e07906b270e545c8350e3608065
Lukman Mudi <lukman.mudi@flexisaf.com>
2022-03-10 09:47:12 +0100

    Code Refactor

--- src/main/java/com/kontrol/events/model/EventDTO.java
+++ src/main/java/com/kontrol/events/model/EventDTO.java
@@ -1,5 +1,8 @@
 package com.kontrol.events.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -8,8 +11,9 @@ import java.util.UUID;
 public class EventDTO {
     public UUID eventId = UUID.randomUUID();
     @NotBlank
-    public String message;
+    public String name;
     @NotNull
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     public LocalDateTime createdAt;
     @NotBlank
     public String source;
@@ -18,7 +22,7 @@ public class EventDTO {
     public String toString() {
         return "EventDTO{" +
                 "eventId=" + eventId +
-                ", message='" + message + '\'' +
+                ", name='" + name + '\'' +
                 ", createdAt=" + createdAt +
                 ", source='" + source + '\'' +
                 '}';
--- src/main/java/com/kontrol/websockets/model/Notification.java
+++ src/main/java/com/kontrol/websockets/model/Notification.java
@@ -1,18 +1,23 @@
 package com.kontrol.websockets.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Notification {
-    public UUID id;
-    public String action;
+    public UUID id = UUID.randomUUID();
+    public String message;
+    public Object payload;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     public LocalDateTime triggeredAt;
 
     @Override
     public String toString() {
         return "Notification{" +
                 "id=" + id +
-                ", action='" + action + '\'' +
+                ", message='" + message + '\'' +
                 ", triggeredAt=" + triggeredAt +
                 '}';
     }
